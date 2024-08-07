3b5deaed01703afe306e96333452f0b5747b5ce0
Vijayalakshmi <vkrish0904@gmail.com>
2024-08-07 10:46:15 +0100

    changes to the config and gitignore file

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl : 'https://homes.midlandheart.org.uk/HomePage.aspx',
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
